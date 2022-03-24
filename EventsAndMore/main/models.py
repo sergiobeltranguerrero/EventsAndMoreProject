366eae64b95d392f8faf91f3373385010a7353f5
Dani <dbistuer@msn.com>
2022-03-24 21:56:34 +0100

    adding models

--- EventsAndMore/main/models.py
+++ EventsAndMore/main/models.py
@@ -1,3 +1,21 @@
+from django.db.models import Model
 from django.db import models
 
 # Create your models here.
+
+class Servicios(Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=500)
+
+class Sector(Model):
+    nombre = models.CharField(max_length=50)
+
+class Incidencia(Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=500)
+    #usuario = models.ForeignKey(Usuario)
+
+class Comentario(Model):
+    incidencia = models.ForeignKey(Incidencia,on_delete=models.DO_NOTHING)
+    asunto = models.CharField(max_length=100)
+    cuerpo = models.CharField(max_length=500)
