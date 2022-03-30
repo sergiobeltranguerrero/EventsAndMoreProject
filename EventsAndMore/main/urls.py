edc81f0d14e144a200eed10f8361375f09621a60
Sergio Beltrán <sergiobeltranguerrero@gmail.com>
2022-03-30 08:03:28 +0200

    eliminado contenido de la rama de sergio

--- EventsAndMore/main/urls.py
+++ EventsAndMore/main/urls.py
@@ -5,6 +5,4 @@ from main.views import RegisterClientView, AdminView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('register/client', RegisterClientView, name='register_client'),
-    path('panel/admin', AdminView.as_view(), name='admin_panel'),
 ]
--- EventsAndMore/main/views.py
+++ EventsAndMore/main/views.py
@@ -2,23 +2,3 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
-
-from main.forms import RegisterClientForm
-
-
-def RegisterClientView(request):
-    if request.method == 'POST':
-        form = RegisterClientForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = RegisterClientForm()
-    return render(request, 'registerClient.html', {'form': form})
-
-
-@method_decorator(login_required, name='dispatch')
-class AdminView(TemplateView):
-    template_name = 'admin.html'
