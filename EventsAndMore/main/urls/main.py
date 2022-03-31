7eb8c5e7a775348a396f1ed84a458b3863767693
Sergio Beltrán <sergiobeltranguerrero@gmail.com>
2022-03-31 09:40:26 +0200

    modulos creados

--- EventsAndMore/main/urls/main.py
+++ EventsAndMore/main/urls/main.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from main.views import RegisterClientView, AdminView
-
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
