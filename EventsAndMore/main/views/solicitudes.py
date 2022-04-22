a9598394e905f831b75eb15bbeba81e268b94b9b
AdriaRojas <opolo999@hotmail.es>
2022-04-22 10:57:34 +0200

    detalles de la assignacion finalizados

--- EventsAndMore/main/views/solicitudes.py
+++ EventsAndMore/main/views/solicitudes.py
@@ -28,15 +28,29 @@ def detalles_assignacion(request,id_assignacion):
     for estado in Assignacion.ESTADO:
         states.append(State(estado[0], estado[1]))
     if request.method == 'POST':
-        assignaciones = Assignacion.objects.get(id=id_assignacion)
-        assignaciones.comentario = str(request.POST['comentario'])
-        assignaciones.save()
-        assignaciones2 = Assignacion.objects.filter(id=id_assignacion)
-        return render(request, "assignacion/detalles_assignacion.html", {"assignaciones": assignaciones2, 'cliente' : assignaciones2[0].cliente,'comentario' : assignaciones2[0].id,'states': states})
+        if request.POST['Valor'] == 'RC':
+            assignaciones = Assignacion.objects.get(id=id_assignacion)
+            assignaciones.estado = 'RC'
+            assignaciones.save()
+            assignaciones2 = Assignacion.objects.filter(id=id_assignacion)
+            return render(request, "assignacion/detalles_assignacion.html",{"assignaciones": assignaciones2, 'cliente': assignaciones2[0].cliente,'comentario': assignaciones2[0].id, 'states': states})
+        if request.POST['Valor'] == 'AP':
+            assignaciones = Assignacion.objects.get(id=id_assignacion)
+            assignaciones.estado = 'AP'
+            assignaciones.save()
+            assignaciones2 = Assignacion.objects.filter(id=id_assignacion)
+            return render(request, "assignacion/detalles_assignacion.html",
+                          {"assignaciones": assignaciones2, 'cliente': assignaciones2[0].cliente,
+                           'comentario': assignaciones2[0].id, 'states': states})
+
+        if request.POST['Valor'] == 'Comentario':
+            assignaciones = Assignacion.objects.get(id=id_assignacion)
+            assignaciones.comentario = str(request.POST['comentario'])
+            assignaciones.save()
+            assignaciones2 = Assignacion.objects.filter(id=id_assignacion)
+            return render(request, "assignacion/detalles_assignacion.html", {"assignaciones": assignaciones2, 'cliente' : assignaciones2[0].cliente,'comentario' : assignaciones2[0].id,'states': states})
+
     else:
         assignaciones = Assignacion.objects.filter(id=id_assignacion)
         return render(request, "assignacion/detalles_assignacion.html", {"assignaciones": assignaciones, 'cliente' : assignaciones[0].cliente,'comentario' : assignaciones[0].id,'states': states})
 
-#def listStock(request):
-#   productes = Stock.objects.filter(floristeria=request.user.floristeria)
-#  return render(request, "Stock/list.html", {"productes": productes})
